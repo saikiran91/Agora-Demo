2091b2b9aff7f87e5b683189e118949b55e5c03f
Sai-Desktop <saikiranmc@gmail.com>
2018-04-20 19:45:28 +0530

    Added agorabase(OpenLive for Android) as a module to the project

---
+++ agorabase/src/main/java/io/agora/agorabase/openlive/ui/VideoUserStatusHolder.java
@@ -0,0 +1,9 @@
+package io.agora.agorabase.openlive.ui;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class VideoUserStatusHolder extends RecyclerView.ViewHolder {
+    public VideoUserStatusHolder(View v) {
+        super(v);
+    }
+}
---
+++ agorabase/src/main/java/io/agora/agorabase/openlive/ui/VideoViewEventListener.java
@@ -0,0 +1,6 @@
+package io.agora.agorabase.openlive.ui;
+import android.view.View;
+
+public interface VideoViewEventListener {
+    void onItemDoubleClick(View v, Object item);
+}
